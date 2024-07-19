a8a030ea9c61a31bb49728b626943c29e2db8ba6
mlsrinivas446 <154888229+mlsrinivas446@users.noreply.github.com>
2024-07-19 19:24:39 +0530

    added features including add multiple todo at a time, edit saved todo, delete todo

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,20 +1,45 @@
-// Write your code here
 import './index.css'
 
-const TodoItem = props => {
-  const {TodosListItem, deleteTodo} = props
-  const {id, title} = TodosListItem
-
-  const deleteTodoItem = () => {
-    deleteTodo(id)
-  }
+const TodoItem = ({todo, toggleEditTodo, toggleCompletion, deleteTodo}) => {
+  const {id, title, isEdit, isCompleted} = todo
 
   return (
     <li className="todo-item-container">
-      <p className="title-name">{title}</p>
-      <button className="button" onClick={deleteTodoItem} type="button">
-        Delete
-      </button>
+      <div className="todo-details">
+        <input
+          type="checkbox"
+          checked={isCompleted}
+          onChange={() => toggleCompletion(id)}
+        />
+        {isEdit ? (
+          <input type="text" value={title} className="todo-edit-input" />
+        ) : (
+          <p className={`title-name ${isCompleted ? 'completed' : ''}`}>
+            {title}
+          </p>
+        )}
+      </div>
+
+      <div className="todo-actions">
+        {isEdit ? (
+          <button
+            className="button save-button"
+            onClick={() => toggleEditTodo(id)}
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            className="button edit-button"
+            onClick={() => toggleEditTodo(id)}
+          >
+            Edit
+          </button>
+        )}
+        <button className="button delete-button" onClick={() => deleteTodo(id)}>
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
